3e3b6b8f8d39c62599c888f50732bde9fc9c204f
KrishnaPermal <krishna.tantalidespermalnaiken@gmail.com>
2019-10-02 14:55:26 +0400

    second krish

--- includes/piedpage.php
+++ includes/piedpage.php
@@ -1,45 +1,110 @@
 <footer id="piedpage">
 
 
-        <div class="container">
-        <div class="row">
-            <div class="card  border-5 bg-transparent bg-dark" style="width: 18rem;">
-                <div class="card-body">
-                    <h1 class="card-title">Introuvable</h1>
-                    <p class="card-text">Nous sommes désolés, nous n'avons pas pu trouver ce que vous recherchiez.</p>
-                    <p class="card-text">Vous pouvez effectuer une recherche ou revenir à la <a href="">page
-                            d'accueil</a>.</p>
-                    <p class="card-text">Si vous suiviez un lien depuis un document existant, vous pouvez vous connecter
-                        et créer la page. Autrement, veuillez <a href="">rapportez un bug.</a> Merci !</p>
-                </div>
-            </div>
+        
+            <!-- Footer -->
+<footer class="page-footer font-small bg-dark pt-4">
+
+<!-- Footer Links -->
+<div class="container text-center text-md-left">
 
 
 
-            <div class="card  border-5 bg-transparent" style="width: 18rem;">
-                <div class="card-body">
-                    <h1 class="card-title">MDN</h1>
-                    <p class="card-text">Nous sommes désolés, nous n'avons pas pu trouver ce que vous recherchiez.</p>
-                    <p class="card-text">Vous pouvez effectuer une recherche ou revenir à la <a href="">page
-                            d'accueil</a>.</p>
-                    <p class="card-text">Si vous suiviez un lien depuis un document existant, vous pouvez vous connecter
-                        et créer la page. Autrement, veuillez <a href="">rapportez un bug.</a> Merci !</p>
-                </div>
-            </div>
 
+  <!-- Grid row -->
+  <div class="row">
+    <!-- Grid column -->
+    <div class="col-md-4 mx-auto">
+      <!-- Content -->
+      <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Footer Content</h5>
+      <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+        consectetur
+        adipisicing elit.</p>
 
-            <div class="card  border-5 bg-transparent" style="width: 18rem;">
-                <div class="card-body">
-                    <h1 class="card-title">Introuvable</h1>
-                    <p class="card-text">Nous sommes désolés, nous n'avons pas pu trouver ce que vous recherchiez.</p>
-                    <p class="card-text">Vous pouvez effectuer une recherche ou revenir à la <a href="">page
-                            d'accueil</a>.</p>
-                    <p class="card-text">Si vous suiviez un lien depuis un document existant, vous pouvez vous connecter
-                        et créer la page. Autrement, veuillez <a href="">rapportez un bug.</a> Merci !</p>
-                </div>
-            </div>
-        </div>
     </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-2 mx-auto"> 
+    <!-- Grid column -->
+  </div>
+  <!-- Grid row -->
+</div>
+
+
+
+
+
+<!-- Footer Links -->
+
+<hr>
+
+<!-- Call to action -->
+<ul class="list-unstyled list-inline text-center py-2">
+  <li class="list-inline-item">
+    <h5 class="mb-1">Register for free</h5>
+  </li>
+  <li class="list-inline-item">
+    <a href="#!" class="btn btn-danger btn-rounded">Sign up!</a>
+  </li>
+</ul>
+<!-- Call to action -->
+
+<hr>
+
+<!-- Social buttons -->
+<ul class="list-unstyled list-inline text-center">
+  <li class="list-inline-item">
+    <a class="btn-floating btn-fb mx-1">
+      <i class="fab fa-facebook-f"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-tw mx-1">
+      <i class="fab fa-twitter"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-gplus mx-1">
+      <i class="fab fa-google-plus-g"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-li mx-1">
+      <i class="fab fa-linkedin-in"> </i>
+    </a>
+  </li>
+  <li class="list-inline-item">
+    <a class="btn-floating btn-dribbble mx-1">
+      <i class="fab fa-dribbble"> </i>
+    </a>
+  </li>
+</ul>
+<!-- Social buttons -->
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2019 Copyright:
+  <a href="https://mdbootstrap.com/education/bootstrap/"> Femmes974</a>
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+            
+
+        
+   
 
 
     </footer>
