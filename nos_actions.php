280cb06aa05c5f71d4e70d07e58311fbf66ab1db
Benjamin <benjulienne0@gmail.com>
2019-10-07 13:32:30 +0400

    modif css

--- nos_actions.php
+++ nos_actions.php
@@ -34,65 +34,65 @@
 
                 <h5 id="item-1">2018</h4>
                     <p> <span class="datage"> Novembre : </span> Marche NousToutes974 : RDV 10h St Gilles les bains Ermitage- (annulée) <br>
-                    <span class="datage">20 Novembre : </span> Conférence de presse NousToutes974 : Saint Denis 10h30 : Communiqué <br>
-                    <span class="datage"> 8 Novembre : </span> AG - Lancement mouvement NousToutes 974 - 24 novembre 2018 : Flyer<br>
-                    <span class="datage">19 mai : </span> Soirée courts métrages avec "ZétOIles" à la Cerise - St Paul : Programme <br>
-                    <span class="datage">3 mai : </span> Assemblée générale extraordinaire Femmes974 <br>
-                    <span class="datage"> 15 Mars : </span> Préparation du lancement du festival ZétOILES page <a href="#"> facebook</a> <br>
-                    <span class="datage">8 Mars : </span> Participation à la soirée"6 femmes à l'honneur" avec AFECT à Saint Benoît
+                        <span class="datage">20 Novembre : </span> Conférence de presse NousToutes974 : Saint Denis 10h30 : <a href="pdf/Communique-noustoutes974-1.pdf">Communiqué</a> <br>
+                        <span class="datage"> 8 Novembre : </span> AG - Lancement mouvement NousToutes 974 - 24 novembre 2018 : <a href="pdf/NousToutes FLYERS 974.pdf">Flyer</a><br>
+                        <span class="datage">19 mai : </span> Soirée courts métrages avec "ZétOIles" à la Cerise - St Paul : <a href="pdf/Programme- zetoiles.jpeg">Programme </a> <br>
+                        <span class="datage">3 mai : </span> Assemblée générale extraordinaire Femmes974 <br>
+                        <span class="datage"> 15 Mars : </span> Préparation du lancement du festival ZétOILES page <a href="https://www.facebook.com/festival.films.femmes974/"> facebook</a> <br>
+                        <span class="datage">8 Mars : </span> Participation à la soirée"6 femmes à l'honneur" avec AFECT à Saint Benoît
                     </p>
                     <h5 id="item-1-1">2017</h5>
                     <p>
-                    <span class="datage">20 Novembre : </span> RDV "O Café" rencontre réseau d'associations à St Denis Va Piano <br>
-                    <span class="datage">7 Novembre : </span> Manifestation contre les inégalités de salaires : RDV St Paul front de mer <br>
-                    <span class="datage">6 avril : </span> Atelier "mieux connaître les métiers de l'audiovisuel et du cinéma" Université reportage <br>
-                    <span class="datage"> 8 Mars : </span> Les pionnières françaises - Lycée Ambroise Vollard - St Pierre - reportage <br>
-                    <span class="datage"> 3 Mars : </span> Réunion intermédiaire CA suivi plan d'actions - Saint Gilles les bains <br>
-                    <span class="datage"> 23 Fevrier : </span> Emission les témoins d'outre mer France O: interview Marianne Grossesses précoces <br>
-                    <span class="datage"> 15 Fevrier : </span> Radio PIKAN (Etang Salé les bains) : interview Marianne
+                        <span class="datage">20 Novembre : </span> RDV "O Café" rencontre réseau d'associations à St Denis Va Piano <br>
+                        <span class="datage">7 Novembre : </span> Manifestation contre les inégalités de salaires : RDV St Paul front de mer <br>
+                        <span class="datage">6 avril : </span> Atelier "mieux connaître les métiers de l'audiovisuel et du cinéma" Université <a href="http://www.femmes974.info/Femmes974-actualites/6AVRIL-2017-femmes974.html"> reportage </a> <br>
+                        <span class="datage"> 8 Mars : </span> Les pionnières françaises - Lycée Ambroise Vollard - St Pierre -<a href="http://www.femmes974.info/Femmes974-actualites/8MARS-2017-femmes974.html"> reportage </a><br>
+                        <span class="datage"> 3 Mars : </span> Réunion intermédiaire CA suivi plan d'actions - Saint Gilles les bains <br>
+                        <span class="datage"> 23 Fevrier : </span> Emission les témoins d'outre mer France O: interview Marianne Grossesses précoces <br>
+                        <span class="datage"> 15 Fevrier : </span> Radio PIKAN (Etang Salé les bains) : interview Marianne
 
 
                     </p>
                     <h5 id="item-1-2">2016</h5>
                     <p>
-                    <span class="datage"> 9 Décembre : </span> Rassemblement laïque à Saint Pierre - les grandes dates des droits des femmes - reportage <br>
-                    <span class="datage"> 25-26 Novembre : </span> Etats Généraux des violences faites aux femmes - reportage <br>
-                    <span class="datage"> 16 Septembre : </span> Réunion intermédiaire CA suivi plan d'actions - Saint Gilles les bains <br>
-                    <span class="datage"> 2 Mai : </span> Assemblée générale ordinaire - reportage <br>
-                    <span class="datage"> 8 mars : </span> Femmes digitales avec la Cité des métiers : Les pionnières (télécharger le pdf), l'évolution du statut des femmes, et l'atelier des réalistarices "le clap" avec Le ballet des arrosoirs, terre marronne, dan fon dmon ker. reportage
+                        <span class="datage"> 9 Décembre : </span> Rassemblement laïque à Saint Pierre - les grandes dates des droits des femmes - <a href="http://www.femmes974.info/Femmes974-actualites/9decembre-2016-femmes974.html"> reportage </a><br>
+                        <span class="datage"> 25-26 Novembre : </span> Etats Généraux des violences faites aux femmes - <a href="http://www.femmes974.info/Femmes974-actualites/Femmes-EGVF-femmes974.html"> reportage </a><br>
+                        <span class="datage"> 16 Septembre : </span> Réunion intermédiaire CA suivi plan d'actions - Saint Gilles les bains <br>
+                        <span class="datage"> 2 Mai : </span> Assemblée générale ordinaire - <a href="http://www.femmes974.info/Femmes974-actualites/AGO-2016-femmes974.html"> reportage </a><br>
+                        <span class="datage"> 8 mars : </span> Femmes digitales avec la Cité des métiers : <a href="pdf/pionnieres-2016.pdf"> Les pionnières (télécharger le pdf)</a>, l'évolution du statut des femmes, et l'atelier des réalistarices "le clap" avec Le ballet des arrosoirs, terre marronne, dan fon dmon ker. <a href="http://www.femmes974.info/Femmes974-actualites/Femmes-digitales-2016.html"> reportage </a>
                     </p>
                     <h5 id="item-2">2015</h4>
 
                         <p>
-                        <span class="datage"> 21 Octobre : </span> Séminaire "FEMMES LEADERS" organisé conjointement avec l'université de la Réunion : reportage et compte rendu <br>
-                        <span class="datage"> 8 Octobre : </span> Préparation du Séminaire "FEMMES LEADERS" : programme <br>
-                        <span class="datage"> 1-2 Octobre : </span> Femmes 974 au Carreco, carrefour des communes de la Réunion : reportage <br>
-                        <span class="datage"> Juin : </span> Préparation du séminaire "femmes leaders 974" 21 octobre 2015 : Donnez votre avis <br>
-                        <span class="datage"> 15 Mai : </span> Assemblée générale ordinaire 15 mai 2015 - reportage <br>
-                        <span class="datage"> 6 Mars : </span> Femmes digitales avec la Cité des métiers et projection de "Miss representation" reportage <br>
-                        <span class="datage"> 30 Janvier : </span> Les trophées des femmes précieuses : une table Femmes 974 - reportage
+                            <span class="datage"> 21 Octobre : </span> Séminaire "FEMMES LEADERS" organisé conjointement avec l'université de la Réunion : <a href="http://www.femmes974.info/Femmes974-actualites/Femmes-leaders-femmes974.html"> reportage </a> et <a href="http://www.femmes974.info/Femmes974-actualites/Femmes-leaders-femmes974.html"> compte rendu </a> <br>
+                            <span class="datage"> 8 Octobre : </span> Préparation du Séminaire "FEMMES LEADERS" : <a href="pdf/Programme Femmes leader-ok.pdf"> programme </a><br>
+                            <span class="datage"> 1-2 Octobre : </span> Femmes 974 au Carreco, carrefour des communes de la Réunion : <a href="http://www.femmes974.info/Femmes974-actualites/femmes974-carreco-2015.html"> reportage </a> <br>
+                            <span class="datage"> Juin : </span> Préparation du séminaire "femmes leaders 974" 21 octobre 2015 : <a href="pdf/Questionnaire-F974- V3-ok.pdf"> Donnez votre avis </a><br>
+                            <span class="datage"> 15 Mai : </span> Assemblée générale ordinaire 15 mai 2015 - <a href="http://www.femmes974.info/Femmes974-actualites/AGO-2015-femmes974.html"> reportage </a><br>
+                            <span class="datage"> 6 Mars : </span> Femmes digitales avec la Cité des métiers et projection de "Miss representation" <a href="http://www.femmes974.info/Femmes974-actualites/Femmes-digitales-2015.html"> reportage </a> <br>
+                            <span class="datage"> 30 Janvier : </span> Les trophées des femmes précieuses : une table Femmes 974 - <a href="http://www.femmes974.info/Femmes974-actualites/F974-trophee-precieuses.html"> reportage</a>
                         </p>
                         <h5 id="item-3">2014</h4>
                             <p>
-                            <span class="datage"> 17 Decembre : </span> Plan d'Action Régional en faveur de l'entreprenariat féminin <br>
-                            <span class="datage"> 23 Mai : </span> Assemblée générale ordinaire 23 mai 2014 - reportage <br>
-                            <span class="datage"> 8 Mars : </span> Evenement en partenariat avec le Planning familial St Paul - reportage <br>
-                            <span class="datage"> 7 Mars : </span> 1ère Projection du documentaire "Miss representation" version française soutenu par la DRDFE et organisée conjointement avec l'Université . <br>
-                            <span class="datage"> noDate : </span> Programme mentotutoral "Femmes974" pour accompagner les femmes - inscrivez-vous
+                                <span class="datage"> 17 Decembre : </span> Plan d'Action Régional en faveur de l'entreprenariat féminin <br>
+                                <span class="datage"> 23 Mai : </span> Assemblée générale ordinaire 23 mai 2014 - reportage <br>
+                                <span class="datage"> 8 Mars : </span> Evenement en partenariat avec le Planning familial St Paul - reportage <br>
+                                <span class="datage"> 7 Mars : </span> 1ère Projection du documentaire "Miss representation" version française soutenu par la DRDFE et organisée conjointement avec l'Université . <br>
+                                <span class="datage"> noDate : </span> Programme mentotutoral "Femmes974" pour accompagner les femmes - inscrivez-vous
                             </p>
                             <h5 id="item-3-1">2013</h5>
                             <p>
-                            <span class="datage"> noDate : </span> Consulter le Rapport de la Commission Europeenne <br>
-                            <span class="datage"> 3 mai : </span> Table ronde "les femmes aux postes à responsabilité"- reportage <br>
-                            <span class="datage"> 5 Avril : </span> Table ronde "les femmes aux postes à responsabilité"- reportage <br>
-                            <span class="datage"> 8 Mars : </span> Article de presse de la Région Réunion le 8 mars 2013 <br>
-                            <span class="datage"> noDate : </span> Alerte à la veille pour le repérage des pubs sexistes - le blog <br>
-                            <span class="datage"> 1er Mars : </span> Table ronde "les femmes aux postes à responsabilité"- reportage
+                                <span class="datage"> noDate : </span> Consulter le Rapport de la Commission Europeenne <br>
+                                <span class="datage"> 3 mai : </span> Table ronde "les femmes aux postes à responsabilité"- reportage <br>
+                                <span class="datage"> 5 Avril : </span> Table ronde "les femmes aux postes à responsabilité"- reportage <br>
+                                <span class="datage"> 8 Mars : </span> Article de presse de la Région Réunion le 8 mars 2013 <br>
+                                <span class="datage"> noDate : </span> Alerte à la veille pour le repérage des pubs sexistes - le blog <br>
+                                <span class="datage"> 1er Mars : </span> Table ronde "les femmes aux postes à responsabilité"- reportage
                             </p>
                             <h5 id="item-3-2">2012</h5>
                             <p>
-                            <span class="datage"> 8 Mars : </span>Journée internationale des femmes à Saint Paul - reportage <br>
-                            <span class="datage"> noDate : </span> Travaux sur la beauté - Leyla ATOUI
+                                <span class="datage"> 8 Mars : </span>Journée internationale des femmes à Saint Paul - reportage <br>
+                                <span class="datage"> noDate : </span> Travaux sur la beauté - Leyla ATOUI
                             </p>
 
             </div>
