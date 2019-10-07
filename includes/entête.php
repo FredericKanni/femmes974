defff25571d07cf81a7e8e1ebe9136b5e01d040d
Benjamin <benjulienne0@gmail.com>
2019-10-07 09:55:26 +0400

    css

--- includes/entête.php
+++ includes/entête.php
@@ -9,7 +9,7 @@
   <div class="navbar-collapse collapse " id="navbarTogglerDemo02" style="">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="accueil.php">Accueil <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="apropos.php">A Propos</a>
--- temoignage.php
+++ temoignage.php
@@ -5,33 +5,55 @@
 
 <body>
 
-    <?php include("includes/navbar.php"); ?>
     <?php include("includes/entête.php"); ?>
 
-    <?php include("menu.php"); ?>
 
     <!-- Le corps -->
 
 
     <div class="container text-center" style="margin-top:50px;">
-        <h1>Les témoignages </h1>
+        <h1 data-toggle="collapse" data-target=".multi-collapse">Les témoignages </h1>
 
-        <p>
-            <a class="btn btn-outline-secondary fondBouton" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Témoignage 1</a>
-            <button class="btn btn-outline-secondary fondBouton" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Témoignage 2</button>
-        </p>
-        <div class="row">
-            <div class="col">
-                <div class="collapse multi-collapse" id="multiCollapseExample1">
-                    <div class="card card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+        <div>
+            <p>
+                <a class="btn btn-outline-secondary fondBouton" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Témoignage 1</a>
+                <button class="btn btn-outline-secondary fondBouton" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Témoignage 2</button>
+            </p>
+            <div class="row">
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample1">
+                        <div class="card card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample2">
+                        <div class="card card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="collapse multi-collapse" id="multiCollapseExample2">
-                    <div class="card card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+        </div>
+        <div class="tem">
+            <p>
+                <a class="btn btn-outline-secondary fondBouton" data-toggle="collapse" href="#multiCollapse1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Témoignage 3</a>
+                <button class="btn btn-outline-secondary fondBouton" type="button" data-toggle="collapse" data-target="#multiCollapse2" aria-expanded="false" aria-controls="multiCollapseExample2">Témoignage 4</button>
+            </p>
+            <div class="row">
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapse1">
+                        <div class="card card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                        </div>
+                    </div>
+                </div class="tem">
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapse2">
+                        <div class="card card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                        </div>
                     </div>
                 </div>
             </div>
